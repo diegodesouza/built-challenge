3077aeb1834be8391ab69d1555ddcef5b0a7e156
Diego de Souza <diegotrus7@gmail.com>
2023-12-20 11:34:54 -0500

    Prettify it a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap';
 import { useStore } from '../stores/BlogStore';
 import { useNavigate } from 'react-router-dom';
+import { ArrowClockwise, FilePlus } from 'react-bootstrap-icons';
 
 const Header = ({ heading = 'My Blog' }) => {
     const store = useStore();
     const navigate = useNavigate();
     const { generateSampleData, handleChange } = store;
+
     const handleCreatePost = () => {
         handleChange('post', {});
         handleChange('editing', false);
         navigate('/posts/create');
     };
+
     return (
         <Navbar>
             <NavbarBrand href='/'>{heading}</NavbarBrand>
-            <Nav pills>
+            <Nav pills fill>
                 <NavItem>
-                    <NavLink onClick={generateSampleData}>Generate Sample Data</NavLink>
+                    <Button color="link" onClick={generateSampleData}
+                             aria-label="Generate Sample Data"
+                    >
+                        <div className="links">
+                            <ArrowClockwise size={25} />
+                        </div>
+                    </Button>
                 </NavItem>
                 <NavItem>
-                    <NavLink onClick={handleCreatePost}>Create Post</NavLink>
+                    <Button color="link" onClick={handleCreatePost} aria-label="Create Post">
+                        <div className="links">
+                            <FilePlus  size={25} />
+                        </div>
+                    </Button>
                 </NavItem>
             </Nav>
         </Navbar>
